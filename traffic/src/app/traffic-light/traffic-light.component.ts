b0ca6310f1f62e82e697a8e62ceb2c1d8bcedfff
batfiowoof <bojidarlambov@abv.bg>
2024-05-19 20:40:15 +0300

    Added panic button and functionalities

--- traffic/src/app/traffic-light/traffic-light.component.ts
+++ traffic/src/app/traffic-light/traffic-light.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-traffic-light',
   standalone: true,
   imports: [],
   templateUrl: './traffic-light.component.html',
-  styleUrl: './traffic-light.component.css'
+  styleUrl: './traffic-light.component.css',
 })
-export class TrafficLightComponent {
+export class TrafficLightComponent implements OnInit {
+  color: string = 'red';
 
+  constructor() {}
+
+  ngOnInit() {
+    this.color = 'red';
+  }
 }
